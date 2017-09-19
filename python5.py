4ffc7c9cfb0adcf4a6c694969823a1b04425da30
Sunny <sunnylee945@hotmail.com>
2017-09-19 23:18:22 +0800

    matplotlib.pyplot

--- python5.py
+++ python5.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def add_layer(inputs, in_size, out_size, activation_function = None):
     Weights = tf.Variable(tf.random_normal([in_size, out_size]))
@@ -25,11 +27,27 @@ loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
         reduction_indices=[1]))
 train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
+# important step
 init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
 
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.scatter(x_data, y_data)
+plt.ion()
+plt.show()
+
 for i in range(1000):
     sess.run(train_step, feed_dict={xs: x_data, ys:y_data})
     if i % 50 == 0:
-        print(sess.run(loss,feed_dict={xs: x_data, ys:y_data}))
+        # to see the step improvement
+        # print(sess.run(loss,feed_dict={xs: x_data, ys:y_data}))
+        try:
+            ax.lines.remove(lines[0])
+        except Exception:
+            pass
+        prediction_value = sess.run(prediction, feed_dict={xs: x_data})
+        lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
+        
+        plt.pause(0.1)
